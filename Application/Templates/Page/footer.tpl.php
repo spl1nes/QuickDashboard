79bd71b5b896c8c56fe682c9ab7464faa53a59d9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-10-05 15:49:49 +0200

    Adding date selector

--- Application/Templates/Page/footer.tpl.php
+++ Application/Templates/Page/footer.tpl.php
@@ -1,6 +1,8 @@
 <footer>
     <div class="floater">
-        based on <a href="https://github.com/Orange-Management" target="_blank">Orange-Management</a>
+    	<div style="display: inline-block; float: left;"><lable for="date-selector">Date Selector:</lable> <input type="date" min="2015-07-01" max="<?= (new \DateTime('now'))->format('Y-m-d'); ?>" id="date-selector" data-action='[{"listener": "change", "action": [{"type": "redirect", "uri": "{%}&t={#date-selector}", "target": "self"}]}]'></div>
+        <div style="display: inline-block; float: right;">based on <a href="https://github.com/Orange-Management" target="_blank">Orange-Management</a></div>
+        <div class="clear"></div>
     </div>
 </footer>
 <script type="text/javascript" src="<?= \phpOMS\Uri\UriFactory::build('{/base}{/rootPath}Application/js/logic.js'); ?>"></script>
