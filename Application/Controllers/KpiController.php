52607f1889e0d4f434e6819a85d1dec53e39ca03
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-10-13 21:13:50 +0200

    Preparing for KPIs

--- Application/Controllers/KpiController.php
+++ Application/Controllers/KpiController.php
@@ -11,5 +11,35 @@ use QuickDashboard\Application\Models\StructureDefinitions;
 
 class KpiController extends DashboardController
 {
+    public function showFinance(RequestAbstract $request, ResponseAbstract $response)
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/QuickDashboard/Application/Templates/Kpi/kpi-finance');
 
+        return $view;
+    }
+
+    public function showMarketing(RequestAbstract $request, ResponseAbstract $response)
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/QuickDashboard/Application/Templates/Kpi/kpi-marketing');
+
+        return $view;
+    }
+
+    public function showPersonnel(RequestAbstract $request, ResponseAbstract $response)
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/QuickDashboard/Application/Templates/Kpi/kpi-personnel');
+
+        return $view;
+    }
+
+    public function showQuality(RequestAbstract $request, ResponseAbstract $response)
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/QuickDashboard/Application/Templates/Kpi/kpi-quality');
+
+        return $view;
+    }
 }
